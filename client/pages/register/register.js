43e59722801686dd4b4b2cb7e1b1f8cfb8224b62
Jesus L <jesus@gravitywell.co.uk>
2018-10-15 18:31:22 +0100

    Validate account in register form in first step

--- client/pages/register/register.js
+++ client/pages/register/register.js
@@ -6,9 +6,11 @@ import { Accounts } from 'meteor/accounts-base'
 
 Template.register.onCreated(() => {
   const template = Template.instance();
+
   template.errorMessage = new ReactiveVar(null);
   template.useExistingAccount = new ReactiveVar(false);
   template.useKeyStoreFile = new ReactiveVar(null);
+  template.isLoading = new ReactiveVar(false);
 });
 
 Template.register.helpers({
@@ -27,60 +29,72 @@ Template.register.events({
   'submit form': function(event, templateInstance) {
     event.preventDefault();
 
-    let keyStoreFile, keyStorePassword, privateKey;
+    let keyStoreFile, keyStorePassword, privateKey, account;
 
     templateInstance.errorMessage.set(null);
+    templateInstance.isLoading.set(true);
 
     if (templateInstance.useExistingAccount.get()) {
       if (templateInstance.useKeyStoreFile.get()) {
         keyStoreFile = $('[name=keystore-file]').prop('files');
         keyStorePassword = $('[name=keystore-password]').val();
+        
         if (!keyStoreFile || !keyStorePassword) {
-          // @todo: Check with this data we can create an account.
           templateInstance.errorMessage.set('Keystore file and password fields are required.');
           return;
         }
+
+        const fr = new FileReader();
+        fr.readAsText(keyStoreFile[0]);
+        fr.onload = function(fileContent) {
+          keyStoreFile = fileContent.target.result;
+          // console.log({ keyStoreFile, keyStorePassword });
+          Meteor.call("loadAccountFromKeystore", keyStoreFile, keyStorePassword, (err, res) =>  {
+            if (err) {
+              templateInstance.errorMessage.set(err);
+            }
+            account = res;
+          });
+        };
       } else {
         privateKey = $('#private-key').val();
         if (!privateKey) {
-          // @todo: Check with this data we can create an account.
           templateInstance.errorMessage.set('Private key field is required.');
           return;
         }
+        Meteor.call("loadAccountFromPrivateKey", privateKey, (err, res) =>  {
+          if (err) {
+            templateInstance.errorMessage.set(err);
+          }
+          account = res;
+        });
       }
     }
     
-    const email = $('[name=email]').val();
-    const password = $('[name=password]').val();
-    const username = $('[name=username]').val();
+    if (!templateInstance.errorMessage.get()) {
+      const email = $('[name=email]').val();
+      const password = $('[name=password]').val();
+      const username = $('[name=username]').val();
 
-    Accounts.createUser({
-      username: username,
-      email: email,
-      password: password
-    }, error => {
-      if (error){
-        templateInstance.errorMessage.set(error.reason);
-      } else {
-        if (templateInstance.useExistingAccount.get()) {
-          if (templateInstance.useKeyStoreFile.get()) {
-            const fr = new FileReader();
-            fr.readAsText(keyStoreFile[0]);
-            fr.onload = function(fileContent) {
-              keyStoreFile = fileContent.target.result;
-              console.log({ keyStoreFile, keyStorePassword });
-              Meteor.call("associateAccount", email, { keyStoreFile, keyStorePassword }, (err, res) =>  {
-                if (err) {
-                  templateInstance.errorMessage.set(err);
-                }
-                else {
-                  FlowRouter.go("home");
-                }
-              });
-            };
-            
+      Accounts.createUser({
+        username: username,
+        email: email,
+        password: password
+      }, error => {
+        if (error){
+          templateInstance.errorMessage.set(error.reason);
+        } else {
+          if (templateInstance.useExistingAccount.get()) {
+            Meteor.call("associateAccount", email, account, (err, res) =>  {
+              if (err) {
+                templateInstance.errorMessage.set(err);
+              }
+              else {
+                FlowRouter.go("home");
+              }
+            });
           } else {
-            Meteor.call("associateAccount", email, { privateKey }, (err, res) =>  {
+            Meteor.call("createAccount", email, (err, res) =>  {
               if (err) {
                 templateInstance.errorMessage.set(err);
               }
@@ -89,18 +103,9 @@ Template.register.events({
               }
             });
           }
-        } else {
-          Meteor.call("createAccount", email, (err, res) =>  {
-            if (err) {
-              templateInstance.errorMessage.set(err);
-            }
-            else {
-              FlowRouter.go("home");
-            }
-          });
         }
-      }
-    });
+      });
+    }
   },
 
   'click #register-existing': function(event, templateInstance) {
